7924da51b6a61e4842aa3d1713a1ca0d2f96699d
JunsangLee <juoc03@gmail.com>
2024-05-22 23:29:16 +0900

    1

--- app/src/main/java/com/flius/guidy/Register_post.kt
+++ app/src/main/java/com/flius/guidy/Register_post.kt
@@ -1,6 +1,8 @@
 package com.flius.guidy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Register_post : AppCompatActivity() {
@@ -8,5 +10,11 @@ class Register_post : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_register_post)
+
+        val postbtn: Button = findViewById(R.id.btn_submit)
+        postbtn.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
